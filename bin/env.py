1e9c8c04876bbaf5cdc2a57c0e962ce7055cfb16
Chin-Jung Hsu <chsu6@ncsu.edu>
2014-05-22 23:00:38 -0500

    Support of Hadoop 2.4.

---
+++ bin/env.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import logging
+
+
+def init(format=None, debug=False):
+    sys.path.append(get_lib_path())
+
+    FORMAT = '%(filename)s:%(lineno)s:%(funcName)s() - %(message)s' if format is None else format
+    logging.getLogger("requests").setLevel(logging.CRITICAL)
+    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO, format=FORMAT)
+
+def get_project_path():
+    return os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+def get_lib_path():
+    return os.path.join(get_project_path(), "src")
+
+def get_cluter_config_path(model, num_nodes, num_storages):
+    cluster_config_path = os.path.join(
+        get_project_path(), "setting", "cluster_%s_%sc%ss.py" % (model, num_nodes, num_storages))
+    return cluster_config_path
+
+def get_node_config_path():
+    node_config_path = os.path.join(
+        get_project_path(), "setting", "node_config.py")
+    return node_config_path
+
+def enable_debug():
+    logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
