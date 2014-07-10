30720b864b84de7c90ab1348d6197bb8ead9b278
Chin-Jung Hsu <chsu6@ncsu.edu>
2014-07-09 20:19:17 -0500

    Add parameter support.

--- script/compare_scheduler.py
+++ script/compare_scheduler.py
@@ -10,18 +10,14 @@ from my.experiment.base import *
 from my.experiment import jobfactory
 
 
-def measure(model, schedulers, num_nodes, num_storages, num_jobs, period, output_dir):
+def measure(model, schedulers, num_jobs, period, output_dir,  cluster_config_path, node_config_path, parameters):
 
-    parameters = {
-        'mapreduce.job.reduce.slowstart.completedmaps': '0.8',
-    }
+    parameters = config.parse_config(parameters)
 
-    cluster_config_path = env.get_cluter_config_path(
-        model, num_nodes, num_storages)
-    node_config_path = env.get_node_config_path()
-    setting = HadoopSetting(cluster_config_path, node_config_path, scheduler="Fifo", model=model, num_nodes=num_nodes, num_storages=num_storages, parameters=parameters)
+    setting = HadoopSetting(cluster_config_path, node_config_path, scheduler="Fifo", model=model, parameters=parameters)
 
-    job_list = ["grep", "terasort", "wordcount", "nocomputation", "histogrammovies", "histogramratings", "custommap1"]
+    #job_list = ["grep", "terasort", "wordcount", "nocomputation", "histogrammovies", "histogramratings", "custommap1"]
+    job_list = ["grep", "terasort", "wordcount"]
     job_size_list = ["1GB", "2GB", "4GB"]
     job_timeline = jobfactory.create_jobs(job_list=job_list, job_size_list=job_size_list, num_jobs=num_jobs, period=period)
 
@@ -37,13 +33,14 @@ def main(argv):
     parser.add_argument("-d", "--directory", required=True, help="The output directory")
     parser.add_argument("-m", "--model", default="decoupled", choices=["decoupled", "reference"])
     parser.add_argument("-s", "--scheduler", action='append', help="The scheduler")
-    parser.add_argument("--num_nodes", type=int, default=1, help="The number of computing nodes")
-    parser.add_argument("--num_storages", type=int, default=1, help="The number of storage nodes")
     parser.add_argument("--num_jobs", type=int, default=10, help="The number of jobs")
     parser.add_argument("--period", type=int, default=10, help="The number of jobs")
+    parser.add_argument("--cluster", required=False, default="setting/cluster_config.py", help="The path to the cluster configuration")
+    parser.add_argument("--node", required=False, default="setting/node_config.py", help="The path to the node configuratoin")
+    parser.add_argument("-p", "--parameter", action="append", help="The parameters")
     args = parser.parse_args()
 
-    measure(args.model, args.scheduler, args.num_nodes, args.num_storages, args.num_jobs, args.period, args.directory)
+    measure(args.model, args.scheduler, args.num_jobs, args.period, args.directory, args.cluster, args.node, args.parameter)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
